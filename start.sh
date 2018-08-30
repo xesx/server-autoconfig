62e5c92167c642eaa90a8a7f5cfa46e773d00453
Alexey Alekhin <alekhin@sportxline.com>
2018-08-30 11:30:37 +0300

    docker-compose

--- start.sh
+++ start.sh
@@ -9,6 +9,18 @@ echo "Error while add user ${USERNAME}"
 apt-get update -y
 apt-get upgrade -y
 
+apt-get install curl
 apt-get install docker.io -y
+
+groupadd docker
+usermod -aG docker $USERNAME
+
+apt-get install git -y
+
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 
 #su - $USERNAME
